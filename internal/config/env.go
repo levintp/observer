88ee028edaea779b273a530df6c6cf13e1c25243
Lior Levin <tdm.levin.lior@gmail.com>
2023-06-23 13:50:56 +0300

    Removed unused configuration filename function

--- internal/config/env.go
+++ internal/config/env.go
@@ -4,11 +4,6 @@ import (
 	"github.com/levintp/observer/internal/types"
 )
 
-// Function to try and get the configuration filename from the environment.
-func getFilenameEnv(filename *string) {
-
-}
-
 // Function to populate the configuration from the environment.
 func getConfigurationEnv(conf *types.Config) error {
 	// TODO: populate configuration from environment.
