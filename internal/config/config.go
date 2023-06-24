3c2e0564fe1b9bdd1ed08f5d0ea193c0e679938e
Lior Levin <tdm.levin.lior@gmail.com>
2023-06-24 23:59:51 +0300

    Reordered functions

--- internal/config/config.go
+++ internal/config/config.go
@@ -71,30 +71,6 @@ func buildConfiguration(conf *types.Config) error {
 	return nil
 }
 
-// Function to update names of mapped structures in the configuration.
-func updateNames(conf *types.Config) {
-	// Iterate over stream configuration and update mapped names.
-	for streamName, stream := range conf.Streams {
-		stream.Name = streamName
-		for metricName, metric := range stream.Metrics {
-			metric.Name = metricName
-			for thresholdName, threshold := range metric.Thresholds {
-				threshold.Name = thresholdName
-			}
-		}
-	}
-
-	// Iterate over node configuration an update mapped names.
-	for nodeName, node := range conf.Nodes {
-		node.Name = nodeName
-	}
-}
-
-// Function to validate the parsed configuration.
-func validateConfiguration(conf *types.Config) error {
-	return nil
-}
-
 // Function to get the configuration from the configuration file.
 func readConfigurationFile(filename string, conf *types.Config) error {
 	log.Debugf("Reading configuration from %s", filename)
@@ -127,4 +103,28 @@ func getConfigurationFile() string {
 	}
 
 	return filename
+}
+
+// Function to update names of mapped structures in the configuration.
+func updateNames(conf *types.Config) {
+	// Iterate over stream configuration and update mapped names.
+	for streamName, stream := range conf.Streams {
+		stream.Name = streamName
+		for metricName, metric := range stream.Metrics {
+			metric.Name = metricName
+			for thresholdName, threshold := range metric.Thresholds {
+				threshold.Name = thresholdName
+			}
+		}
+	}
+
+	// Iterate over node configuration an update mapped names.
+	for nodeName, node := range conf.Nodes {
+		node.Name = nodeName
+	}
+}
+
+// Function to validate the parsed configuration.
+func validateConfiguration(conf *types.Config) error {
+	return nil
 }
