e78c902011ec4530d7a9da8cc9cf50aa29363542
Lior Levin <tdm.levin.lior@gmail.com>
2023-07-01 23:54:44 +0300

    Moved configuration type to config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,125 +6,44 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/levintp/observer/internal/log"
-	"github.com/levintp/observer/internal/meta"
 	"github.com/levintp/observer/internal/types"
 	"gopkg.in/yaml.v3"
 )
 
-var cfg *types.Config // Global singleton configuration.
+var cfg *Config // Global singleton configuration.
 
-const environmentPrefix = "OBSERVER_"
-const configurationFileEnv = environmentPrefix + "CONFIG_FILE"
-const configurationFileFlag = "config-file"
-const defaultConfigurationFile = "/etc/observer/observer.yaml"
-
-// Function to get the configuration.
-func Get() *types.Config {
-	if cfg == nil {
-		log.Info("Loading configuration")
-		cfg = &types.Config{}
-		if err := buildConfiguration(cfg); err != nil {
-			log.Fatalf("Failed to load configuration: %v", err)
-		}
-	}
-
-	return cfg
+// The `Config` type is a structure that contains the configuration of the
+// Observer system.
+type Config struct {
+	Controller types.ControllerSpec         `yaml:"controller"`                                                                        // Configuration regarding the controller.
+	Database   types.DatabaseSpec           `yaml:"database"`                                                                          // Configuration regarding the database.
+	Agent      types.AgentSpec              `yaml:"agent"`                                                                             // Configuration regarding the local agent.
+	API        types.APISpec                `yaml:"api"`                                                                               // Configuration regarding the API.
+	PluginDir  string                       `yaml:"plugin_dir" env:"PLUGIN_DIR" flag:"plugin-dir" default:"/usr/lib/observer/plugins"` // Directory of plugin objects.
+	Streams    map[string]*types.StreamSpec `yaml:"streams"`                                                                           // List of streams.
+	Nodes      map[string]*types.NodeSpec   `yaml:"nodes"`                                                                             // List of nodes.
 }
 
-// Function to build a new global configuration.
-func buildConfiguration(cfg *types.Config) error {
-	// Generate minimal default configuration.
-	if err := meta.SetDefaults(cfg); err != nil {
-		return fmt.Errorf("default: %v", err)
-	}
+// Function to convert the `Config` type into a printable, human-readable
+// string by marshaling it back to the configuration file format.
+func (cfg Config) String() string {
+	str, _ := yaml.Marshal(cfg)
+	return string(str)
+}
 
-	// Read configuration from file.
-	err := readConfigurationFile(getConfigurationFile(), cfg)
+// Function to get the specification of the local node.
+func (cfg Config) GetLocalNode() (*types.NodeSpec, error) {
+	// Get hostname of local node.
+	hostname, err := os.Hostname()
 	if err != nil {
-		return fmt.Errorf("file: %v", err)
+		return nil, err
 	}
 
-	// Override configuration with higher priority values from enviroment.
-	if err := meta.SetEnvironment(cfg, environmentPrefix); err != nil {
-		return fmt.Errorf("environment: %v", err)
+	// Iterate over the nodes in the configuration.
+	node, found := cfg.Nodes[hostname]
+	if !found {
+		return nil, fmt.Errorf("node %s not in configuration", hostname)
 	}
 
-	// Override configuration with highest priority values from flags.
-	if err := meta.SetFlags(cfg); err != nil {
-		return fmt.Errorf("commandline: %v", err)
-	}
-
-	// Fill empty fields with default values after configuration expansion.
-	if err := meta.SetDefaults(cfg); err != nil {
-		return fmt.Errorf("post-process: %v", err)
-	}
-
-	// Process configuration.
-	updateNames(cfg)
-
-	// Validate post-processed configuration.
-	if err := validateConfiguration(cfg); err != nil {
-		return fmt.Errorf("invalid configuration: %v", err)
-	}
-
-	return nil
-}
-
-// Function to get the configuration from the configuration file.
-func readConfigurationFile(filename string, cfg *types.Config) error {
-	log.Debugf("Reading configuration from %s", filename)
-
-	// Read the configuration file.
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		return err
-	}
-
-	// Parse the configuration file.
-	err = yaml.Unmarshal(content, cfg)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Function to get the configuration filename from either environment or flags, or default.
-func getConfigurationFile() string {
-	filename := defaultConfigurationFile
-
-	if environFilename := os.Getenv(configurationFileEnv); environFilename != "" {
-		filename = environFilename
-	}
-
-	if flagFilename := meta.GetFlag(configurationFileFlag); flagFilename != "" {
-		filename = flagFilename
-	}
-
-	return filename
-}
-
-// Function to update names of mapped structures in the configuration.
-func updateNames(cfg *types.Config) {
-	// Iterate over stream configuration and update mapped names.
-	for streamName, stream := range cfg.Streams {
-		stream.Name = streamName
-		for metricName, metric := range stream.Metrics {
-			metric.Name = metricName
-			for thresholdName, threshold := range metric.Thresholds {
-				threshold.Name = thresholdName
-			}
-		}
-	}
-
-	// Iterate over node configuration an update mapped names.
-	for nodeName, node := range cfg.Nodes {
-		node.Name = nodeName
-	}
-}
-
-// Function to validate the parsed configuration.
-func validateConfiguration(conf *types.Config) error {
-	return nil
+	return node, nil
 }
