76a8dacd8e3e16fd788c3ba403664990509d6506
Lior Levin <tdm.levin.lior@gmail.com>
2023-07-03 00:55:15 +0300

    Further implemented stream functionalities

--- internal/streams/streams.go
+++ internal/streams/streams.go
@@ -2,6 +2,8 @@ package streams
 
 import (
 	"github.com/levintp/observer/internal/config"
+	"github.com/levintp/observer/internal/log"
+	"github.com/levintp/observer/internal/plugins"
 	"github.com/levintp/observer/internal/types"
 )
 
@@ -9,7 +11,12 @@ import (
 // from the configuration and a target queue for the metrics' samples.
 type Stream struct {
 	spec    *types.StreamSpec
-	samples types.Queue[types.Sample]
+	samples chan types.Sample
+}
+
+// Function to initialize a new `Stream` structure with a given specification.
+func New(spec *types.StreamSpec) Stream {
+	return Stream{spec, make(chan types.Sample, config.Get().Agent.BufferSize)}
 }
 
 // Function to check if the stream should run on the local node.
@@ -23,20 +30,46 @@ func (stream Stream) ShouldRun() bool {
 }
 
 // Function to start running the stream.
-func (steram Stream) Start() error {
-	// TODO:
-	//  - for each metric in the stream:
-	// 	  - get the configured module plugin for the metric.
-	//	  - get the configured function from the module plugin.
-	// 	  - start running the function with the configured arguments in a
-	//		separate go-routine.
-	//	- if any step of the way fails, return an error.
-	return nil
+func (stream Stream) Start() {
+	for _, metric := range stream.spec.Metrics {
+		go stream.metricHandler(metric)
+	}
+}
+
+// Function to handle execution of a metric in the stream.
+func (stream Stream) metricHandler(metric *types.MetricSpec) {
+	// Get the sampling function as configured in the metric specification.
+	samplingFunc, err := plugins.GetSamplingFunc(metric.Module.Plugin, metric.Module.Function)
+	if err != nil {
+		log.Errorw("Failed to obtain sampling function",
+			"metric", metric.Name,
+			"module", metric.Module.Plugin,
+			"symbol", metric.Module.Function)
+	}
+
+	// Run the sampling function.
+	result, err := samplingFunc(metric.Module.Arguments)
+	if err != nil {
+		log.Errorw("Failed to run sampling function",
+			"metric", metric.Name,
+			"module", metric.Module.Plugin,
+			"symbol", metric.Module.Function)
+	}
+
+	// Publish the result of the sampling function execution down-stream.
+	for _, data := range result {
+		stream.publishData(data)
+	}
+
+	// TODO: time interval mechanism.
+}
+
+// Function to publish metric data into the stream.
+func (stream Stream) publishData(data types.MetricData) {
+	stream.samples <- data.ToSample()
 }
 
 // Function to collect a sample from the stream.
-//
-// Alias for `stream.Samples.Pop()`
-func (stream Stream) Collect() (types.Sample, error) {
-	return stream.samples.Pop()
+func (stream Stream) Collect() types.Sample {
+	return <-stream.samples
 }
