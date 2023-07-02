ddca4cf83e6e909db6ee51e059d548054acbb9e3
Lior Levin <tdm.levin.lior@gmail.com>
2023-07-03 00:53:39 +0300

    Added conversion between types `Sample` and `MetricData`

--- internal/types/metric_data.go
+++ internal/types/metric_data.go
@@ -5,3 +5,18 @@ type MetricData struct {
 	Value  float64
 	Labels map[string]any
 }
+
+// Function to convert the metric data into a sample.
+func (data MetricData) ToSample() Sample {
+	sample := make(Sample)
+
+	// Copy the labels map into the sample.
+	for label, value := range data.Labels {
+		sample[label] = value
+	}
+
+	// Set the value of the sample.
+	sample[SampleValueKey] = data.Value
+
+	return sample
+}
--- internal/types/sample.go
+++ internal/types/sample.go
@@ -3,3 +3,5 @@ package types
 // The `Sample` type is a map of generic labels and values that defines a
 // sample of data in a stream.
 type Sample map[string]any
+
+const SampleValueKey = "value"
