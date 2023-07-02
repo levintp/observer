33d6f0ccabd628d74913dc34092d91591b2cf11e
Lior Levin <tdm.levin.lior@gmail.com>
2023-07-02 23:01:57 +0300

    Added buffer size configuration parameter

--- internal/types/agent_spec.go
+++ internal/types/agent_spec.go
@@ -3,7 +3,8 @@ package types
 // The `AgentSpec` type is a structure that defines the configuration
 // specification of the agent component.
 type AgentSpec struct {
-	Port     int    `yaml:"port" env:"AGENT_PORT" flag:"agent-port" default:"1016"`                                       // Connection port to the agent.
-	LogFile  string `yaml:"log_file" env:"DATABASE_AGENT_FILE" flag:"log-file" default:"/var/log/observer/observerd.log"` // Output file of the agent log.
-	LogLevel string `yaml:"log_level" env:"DATABASE_AGENT_LEVEL" flag:"log-level" default:"info"`                         // Level of the agent log.
+	Port       int    `yaml:"port" env:"AGENT_PORT" flag:"agent-port" default:"1016"`                                  // Connection port to the agent.
+	LogFile    string `yaml:"log_file" env:"AGENT_LOG_FILE" flag:"log-file" default:"/var/log/observer/observerd.log"` // Output file of the agent log.
+	LogLevel   string `yaml:"log_level" env:"AGENT_LOG_LEVEL" flag:"log-level" default:"info"`                         // Level of the agent log.
+	BufferSize int    `yaml:"buffer_size" env:"AGENT_BUFFER_SIZE" flag:"buffer-size" default:"4096"`                   // Samples buffer size.
 }
